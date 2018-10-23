5c40d9595171a6b26c07640622017ca16bf2a03e
(s) Charles Whitehair <cjwhitehair@pm7-0090.uopnet.plymouth.ac.uk>
2018-10-23 17:43:43 +0100

    Updated 2

--- 00_Template/sketch.js
+++ 00_Template/sketch.js
@@ -11,16 +11,43 @@ function setup() {
   //Print a message to the console - to view using Chrome:
   //View > Developer > Developer Tools > Console
   console.log("Initialization: OK")
-  createCanvas(500,500);
+  createCanvas(2000,2000);
 }
 
+rand=Math.floor(Math.random() * 256);
+rand2=Math.floor(Math.random() * 256);
+rand3=Math.floor(Math.random() * 256);
 //Rendering function
 function draw() {
   //Set a background color
-  background(255, 0, 255);
+  background(rand, rand2, rand3);
 
   //Print a message to the console - to view using Chrome:
   //View > Developer > Developer Tools > Console
   console.log("Rendering...")
   noLoop();
+}
+function draw() {
+  strokeWeight(10);
+  line(50,60,500,300)
+  line(50,60,50,300)
+  line(50,60,500,60)
+  line(50,300,500,60)
+  line(50,300,500,300)
+  line(500,60,500,300)
+
+  strokeWeight(2);
+
+  fill(0,0,255)
+  ellipse(50,60,80,100)
+
+  fill(255,0,0)
+  ellipse(500,60,80,100)
+
+  fill(0,255,0)
+  ellipse(50,300,80,100)
+
+  fill(255,225,0
+)
+  ellipse(500,300,80,100)
 }
