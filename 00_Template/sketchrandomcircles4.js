ab80e9d4c29cf3bbbd5d1f63c78aac28594a580d
(s) Charles Whitehair <cjwhitehair@pm7-0090.uopnet.plymouth.ac.uk>
2018-11-06 17:32:24 +0000

    Updated

--- 00_Template/sketchrandomcircles4.js
+++ 00_Template/sketchrandomcircles4.js
@@ -1,9 +1,22 @@
+var song;
+
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
+}
+
 function setup() {
   //Print a message to the console - to view using Chrome:
   //View > Developer > Developer Tools > Console
+  song = loadSound('00_Template/libraries/How to Pronounce Egg.mp3');
   console.log("Initialization: OK")
   createCanvas(1920,1080);
-  frameRate(20);
+  frameRate(200);
   noStroke();
 }
 
