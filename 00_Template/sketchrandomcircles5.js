cce6ab4763d1f8b84a4aabf086469b7cd69e62e7
(s) Charles Whitehair <cjwhitehair@pm7-0090.uopnet.plymouth.ac.uk>
2018-11-12 12:50:17 +0000

    Updates 12/11/18

--- 00_Template/sketchrandomcircles5.js
+++ 00_Template/sketchrandomcircles5.js
@@ -3,7 +3,7 @@ function setup() {
   //View > Developer > Developer Tools > Console
   console.log("Initialization: OK")
   createCanvas(1900,1080);
-  frameRate(1);
+  frameRate(10);
   noStroke();
 }
 
@@ -12,9 +12,13 @@ function setup() {
 function draw() {
   //background(40);
 
+  for(let y = mouseY; y<1080; y){
+    for(let x = mouseX; x<1920; x){
+
       fill(random(255),random(255),random(255));
 
-      size = random(20,100)
       rect(mouseX, 0, 5, mouseY);
       rect(0, mouseY, mouseX+5, 5);
+    }
+  }
 }
