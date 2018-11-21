254155e4497f27b82e10a0912547eb8f64613588
Chazster567 <charles.whitehair@students.plymouth.ac.uk>
2018-11-21 15:08:23 +0000

    20/11/2018

--- 00_Template/sketch.js
+++ 00_Template/sketch.js
@@ -1,388 +1,76 @@
-//https://github.com/stavrosdidakis/DAT-GAD-405_2017
-//https://p5js.org
+var r = 127; // r value variable
+var g = 127; // g value variable
+var b = 127; // b value variable
+var time = 0; // time variable
+var button; // button variable
 
-//DAT405 / GAD405
-//00_Template
-//The purpose of this sketch is to provide a basic template
-//for P5.js projects
+function setup() { // sets up the work area
+  createCanvas(841, 594); // sets canvas to specified dimensions
+  noStroke();
 
-//Initialization function
-function setup() {
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Initialization: OK")
-  createCanvas(200,300);
+  button = createButton('Random Colour'); // button creation for random colour
+  button.position(20, 550); // positioning button
+  button.mousePressed(recolour); // calls function
+
+  button = createButton('Red'); // button creation for red
+  button.position(127, 550); // positioning button
+  button.mousePressed(recolourRed); // calls function
+
+  button = createButton('Green'); // button creation for green
+  button.position(173, 550); // positioning button
+  button.mousePressed(recolourGreen); // calls function
+
+  button = createButton('Blue'); // button creation for blue
+  button.position(231, 550); // positioning button
+  button.mousePressed(recolourBlue); // calls function
 }
 
-//Rendering function
-function draw() {
-  noStroke();
-  fill(60,11,15);
-  rect(60,0,10,10)
-  rect(70,0,10,10)
-  rect(80,0,10,10)
-  rect(90,0,10,10)
-  rect(100,0,10,10)
-  rect(110,0,10,10)
-  rect(120,0,10,10)
-  rect(130,0,10,10)
-  rect(50,10,10,10)
-  rect(140,10,10,10)
-  rect(40,20,10,10)
-  rect(150,20,10,10)
-  rect(30,30,10,10)
-  rect(160,30,10,10)
-  rect(20,40,10,10)
-  rect(170,40,10,10)
-  rect(20,50,10,10)
-  rect(170,50,10,10)
-  rect(10,60,10,10)
-  rect(180,60,10,10)
-  rect(10,70,10,10)
-  rect(180,70,10,10)
-  rect(10,80,10,10)
-  rect(180,80,10,10)
-  rect(10,90,10,10)
-  rect(180,90,10,10)
-  rect(10,100,10,10)
-  rect(180,100,10,10)
-  rect(10,110,10,10)
-  rect(180,110,10,10)
-  rect(10,120,10,10)
-  rect(180,120,10,10)
-  rect(10,130,10,10)
-  rect(180,130,10,10)
-  rect(10,140,10,10)
-  rect(180,140,10,10)
-  rect(20,150,10,10)
-  rect(170,150,10,10)
-  rect(20,160,10,10)
-  rect(170,160,10,10)
-  rect(30,170,10,10)
-  rect(160,170,10,10)
-  rect(30,180,10,10)
-  rect(160,180,10,10)
-  rect(40,190,10,10)
-  rect(150,190,10,10)
-  rect(50,200,10,10)
-  rect(140,200,10,10)
-  rect(50,210,10,10)
-  rect(140,210,10,10)
-  rect(60,220,10,10)
-  rect(130,220,10,10)
-  rect(60,230,10,10)
-  rect(130,230,10,10)
-  rect(70,240,10,10)
-  rect(120,240,10,10)
-  rect(80,250,10,10)
-  rect(90,250,10,10)
-  rect(100,250,10,10)
-  rect(110,250,10,10)
+function draw() { // function to produce the objects involved
+  background(0, 10); // creates a black background woth low opacity to leave trails
 
-  fill(143,86,59);
+  // creates ellipse mesh along with the waves
+  for (var y = 0; y <= width; y = y + 100) {
+    for (var x = 0; x <= width; x = x + 10) {
+      // the waves starting point varies on mouse location
+      var angleX = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
+      var angleY = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
+      // and also based on the location of the particle
+      var angle = angleX * (x / width) + angleY * (y / height);
 
-  rect(60,10,10,10)
-  rect(70,10,10,10)
-  rect(80,10,10,10)
-  rect(90,10,10,10)
-  rect(100,10,10,10)
-  rect(110,10,10,10)
-  rect(120,10,10,10)
-  rect(130,10,10,10)
-  rect(70,20,10,10)
-  rect(90,20,10,10)
-  rect(110,20,10,10)
-  rect(120,20,10,10)
-  rect(130,20,10,10)
-  rect(140,20,10,10)
-  rect(120,30,10,10)
-  rect(140,30,10,10)
-  rect(150,30,10,10)
-  rect(110,40,10,10)
-  rect(150,40,10,10)
-  rect(160,40,10,10)
-  rect(140,50,10,10)
-  rect(160,50,10,10)
-  rect(170,60,10,10)
-  rect(170,70,10,10)
-  rect(170,80,10,10)
-  rect(170,90,10,10)
-  rect(170,100,10,10)
-  rect(170,110,10,10)
-  rect(170,120,10,10)
+      // each particle moves in an oscillation
+      var movementX = x + 10 * cos(2 * PI * time + angle);
+      var movementY = y + 10 * sin(2 * PI * time + angle);
 
-  fill(102,58,49);
+      var size = 3 // size variable
+      fill(r, g, b); // fill particle
+      ellipse(movementX, movementY, size, size); // draw particle
+      fill(r, g, b); // fill particle
+      ellipse(movementY, movementX, size, size); // draw particle
+    }
+  }
+  time = time + 0.01; // updates time
+}
 
-  rect(50,20,10,10)
-  rect(60,20,10,10)
-  rect(80,20,10,10)
-  rect(100,20,10,10)
-  rect(40,30,10,10)
-  rect(50,30,10,10)
-  rect(60,30,10,10)
-  rect(70,30,10,10)
-  rect(80,30,10,10)
-  rect(90,30,10,10)
-  rect(100,30,10,10)
-  rect(110,30,10,10)
-  rect(130,30,10,10)
-  rect(30,40,10,10)
-  rect(40,40,10,10)
-  rect(50,40,10,10)
-  rect(60,40,10,10)
-  rect(70,40,10,10)
-  rect(80,40,10,10)
-  rect(90,40,10,10)
-  rect(100,40,10,10)
-  rect(120,40,10,10)
-  rect(130,40,10,10)
-  rect(140,40,10,10)
-  rect(30,50,10,10)
-  rect(40,50,10,10)
-  rect(50,50,10,10)
-  rect(60,50,10,10)
-  rect(70,50,10,10)
-  rect(80,50,10,10)
-  rect(100,50,10,10)
-  rect(110,50,10,10)
-  rect(120,50,10,10)
-  rect(130,50,10,10)
-  rect(150,50,10,10)
-  rect(30,60,10,10)
-  rect(40,60,10,10)
-  rect(50,60,10,10)
-  rect(20,60,10,10)
-  rect(80,60,10,10)
-  rect(100,60,10,10)
-  rect(120,60,10,10)
-  rect(140,60,10,10)
-  rect(150,60,10,10)
-  rect(160,60,10,10)
-  rect(30,70,10,10)
-  rect(40,70,10,10)
-  rect(20,70,10,10)
-  rect(60,70,10,10)
-  rect(70,70,10,10)
-  rect(80,70,10,10)
-  rect(110,70,10,10)
-  rect(120,70,10,10)
-  rect(130,70,10,10)
-  rect(150,70,10,10)
-  rect(160,70,10,10)
-  rect(30,80,10,10)
-  rect(20,80,10,10)
-  rect(50,80,10,10)
-  rect(140,80,10,10)
-  rect(160,80,10,10)
-  rect(20,90,10,10)
-  rect(30,90,10,10)
-  rect(160,90,10,10)
-  rect(20,100,10,10)
-  rect(30,100,10,10)
-  rect(160,100,10,10)
-  rect(20,110,10,10)
-  rect(30,110,10,10)
-  rect(160,110,10,10)
-  rect(20,120,10,10)
-  rect(30,120,10,10)
-  rect(160,120,10,10)
-  rect(30,130,10,10)
-  rect(160,130,10,10)
-  rect(30,140,10,10)
-  rect(160,140,10,10)
-  rect(30,150,10,10)
-  rect(160,150,10,10)
-  rect(30,160,10,10)
-  rect(160,160,10,10)
-  rect(40,170,10,10)
-  rect(150,170,10,10)
-  rect(40,180,10,10)
-  rect(150,180,10,10)
-  rect(50,190,10,10)
-  rect(140,190,10,10)
-  rect(70,190,10,10)
-  rect(120,190,10,10)
-  rect(70,200,10,10)
-  rect(120,200,10,10)
-  rect(70,180,10,10)
-  rect(120,180,10,10)
-  rect(80,180,10,10)
-  rect(110,180,10,10)
-  rect(90,180,10,10)
-  rect(100,180,10,10)
-  rect(90,170,10,10)
-  rect(100,170,10,10)
+function recolour() { // recolours randomly
+  r = random(255); // random r value
+  g = random(255); // random g value
+  b = random(255); // random b value
+}
 
-  fill(238,195,153);
+function recolourRed() { // recolours red
+  r = 255;
+  g = 0;
+  b = 0;
+}
 
-  rect(90,50,10,10)
-  rect(60,60,10,10)
-  rect(70,60,10,10)
-  rect(90,60,10,10)
-  rect(110,60,10,10)
-  rect(130,60,10,10)
-  rect(50,70,10,10)
-  rect(90,70,10,10)
-  rect(100,70,10,10)
-  rect(140,70,10,10)
-  rect(90,80,10,10)
-  rect(150,80,10,10)
-  rect(150,100,10,10)
-  rect(90,110,10,10)
-  rect(100,110,10,10)
-  rect(150,110,10,10)
-  rect(50,120,10,10)
-  rect(90,120,10,10)
-  rect(100,120,10,10)
-  rect(110,120,10,10)
-  rect(140,120,10,10)
-  rect(150,120,10,10)
-  rect(50,130,10,10)
-  rect(60,130,10,10)
-  rect(70,130,10,10)
-  rect(90,130,10,10)
-  rect(100,130,10,10)
-  rect(110,130,10,10)
-  rect(120,130,10,10)
-  rect(130,130,10,10)
-  rect(140,130,10,10)
-  rect(150,130,10,10)
-  rect(20,140,10,10)
-  rect(50,140,10,10)
-  rect(60,140,10,10)
-  rect(70,140,10,10)
-  rect(90,140,10,10)
-  rect(100,140,10,10)
-  rect(110,140,10,10)
-  rect(120,140,10,10)
-  rect(130,140,10,10)
-  rect(140,140,10,10)
-  rect(150,140,10,10)
-  rect(170,140,10,10)
-  rect(50,150,10,10)
-  rect(60,150,10,10)
-  rect(70,150,10,10)
-  rect(90,150,10,10)
-  rect(110,150,10,10)
-  rect(120,150,10,10)
-  rect(130,150,10,10)
-  rect(140,150,10,10)
-  rect(150,150,10,10)
-  rect(50,160,10,10)
-  rect(60,160,10,10)
-  rect(70,160,10,10)
-  rect(80,160,10,10)
-  rect(100,160,10,10)
-  rect(110,160,10,10)
-  rect(120,160,10,10)
-  rect(130,160,10,10)
-  rect(140,160,10,10)
-  rect(150,160,10,10)
-  rect(60,170,10,10)
-  rect(70,170,10,10)
-  rect(80,170,10,10)
-  rect(110,170,10,10)
-  rect(120,170,10,10)
-  rect(130,170,10,10)
-  rect(140,170,10,10)
-  rect(60,180,10,10)
-  rect(130,180,10,10)
-  rect(140,180,10,10)
-  rect(60,190,10,10)
-  rect(130,190,10,10)
-  rect(60,200,10,10)
-  rect(130,200,10,10)
-  rect(80,200,10,10)
-  rect(110,200,10,10)
-  rect(80,190,10,10)
-  rect(110,190,10,10)
-  rect(90,200,10,10)
-  rect(100,200,10,10)
-  rect(70,210,10,10)
-  rect(80,210,10,10)
-  rect(90,210,10,10)
-  rect(100,210,10,10)
-  rect(110,210,10,10)
-  rect(120,210,10,10)
-  rect(80,220,10,10)
-  rect(90,220,10,10)
-  rect(100,220,10,10)
-  rect(110,220,10,10)
+function recolourGreen() { // recolours green
+  r = 0;
+  g = 255;
+  b = 0;
+}
 
-  fill(216,160,101);
-
-  rect(40,80,10,10)
-  rect(80,80,10,10)
-  rect(100,80,10,10)
-  rect(110,80,10,10)
-  rect(40,100,10,10)
-  rect(100,100,10,10)
-  rect(90,100,10,10)
-  rect(40,110,10,10)
-  rect(50,110,10,10)
-  rect(80,110,10,10)
-  rect(110,110,10,10)
-  rect(140,110,10,10)
-  rect(40,120,10,10)
-  rect(60,120,10,10)
-  rect(70,120,10,10)
-  rect(80,120,10,10)
-  rect(120,120,10,10)
-  rect(130,120,10,10)
-  rect(20,130,10,10)
-  rect(40,130,10,10)
-  rect(80,130,10,10)
-  rect(170,130,10,10)
-  rect(40,140,10,10)
-  rect(80,140,10,10)
-  rect(40,150,10,10)
-  rect(80,150,10,10)
-  rect(100,150,10,10)
-  rect(40,160,10,10)
-  rect(90,160,10,10)
-  rect(50,170,10,10)
-  rect(50,180,10,10)
-  rect(60,210,10,10)
-  rect(130,210,10,10)
-  rect(70,220,10,10)
-  rect(120,220,10,10)
-  rect(70,230,10,10)
-  rect(120,230,10,10)
-  rect(80,230,10,10)
-  rect(110,230,10,10)
-  rect(90,230,10,10)
-  rect(100,230,10,10)
-  rect(80,240,10,10)
-  rect(110,240,10,10)
-  rect(90,240,10,10)
-  rect(100,240,10,10)
-
-  fill(209,70,73);
-
-  rect(90,190,10,10)
-  rect(100,190,10,10)
-
-  fill(0,0,0);
-
-  rect(70,90,10,10)
-  rect(130,90,10,10)
-
-  fill(216,228,236);
-
-  rect(40,90,10,10)
-  rect(90,90,10,10)
-  rect(100,90,10,10)
-  rect(150,90,10,10)
-  rect(50,100,10,10)
-  rect(80,100,10,10)
-  rect(110,100,10,10)
-  rect(140,100,10,10)
-  rect(60,110,10,10)
-  rect(70,110,10,10)
-  rect(120,110,10,10)
-  rect(130,110,10,10)
-
-  //View > Developer > Developer Tools > Console
-  console.log("Rendering...")
-  noLoop();
+function recolourBlue() { // recolours blue
+  r = 0;
+  g = 0;
+  b = 255;
 }
